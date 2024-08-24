bb96a96366805a703d898856d08fc166012b2314
Pratik Sonawane <134759546+prats515@users.noreply.github.com>
2024-08-24 11:09:49 +0530

    first deployment

---
+++ jenkinsDeployment/src/test/java/com/jenking/JenkinsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
